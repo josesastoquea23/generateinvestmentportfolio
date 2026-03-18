a6f11d7be0f957c28591dc30f49c36fcdacf0bf3
josesastoquea23 <josesastoquea23@gmail.com>
2026-03-18 16:14:38 -0500

    Add files via upload

---
+++ PortfolioAnalyzer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import PortfolioAnalyzer from './PortfolioAnalyzer'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PortfolioAnalyzer />
+  </React.StrictMode>
+)
